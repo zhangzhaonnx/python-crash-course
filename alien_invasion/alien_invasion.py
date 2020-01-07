1fbbbee90c1daf04f9e1974ae7c77c24a57e4c86
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-07 16:14:44 +0800

    将子弹集成到游戏中

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -16,10 +17,14 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
 
+    # 创建一个用来存储子弹的编组
+    bullets = Group()
+
     # 开始游戏的主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -2,12 +2,12 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Bullet():
+class Bullet(Sprite):
     """一个对飞船发射的子弹进行管理的类"""
 
     def __init__(self, ai_settings, screen, ship):
         """在飞船所处的位置创建一个子弹对象"""
-        super().__init__
+        super().__init__()
         self.screen = screen
 
         # 在(0, 0)创建一个表示子弹的矩形，再设置正确的位置
