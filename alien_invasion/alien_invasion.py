aa1089e4504d46e51305e595dc424d3c3325e887
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-08 21:38:14 +0800

    绘制按钮

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -14,6 +15,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # 创建 Play 按钮
+    play_button = Button(ai_settings, screen, 'Play')
 
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
@@ -36,6 +40,6 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
